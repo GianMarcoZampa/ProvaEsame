c2fda2be9478ba36dc8f33f158b4a4ac7d802c16
ceciu <cecistecconi@gmail.com>
2019-05-30 01:16:30 +0200

    Rewriting post mapping of the controller

--- Itinerant Markets/src/main/java/rest/markets/controllers/ItinerantMarketController.java
+++ Itinerant Markets/src/main/java/rest/markets/controllers/ItinerantMarketController.java
@@ -1,5 +1,7 @@
 package rest.markets.controllers;
 
+import java.util.Vector;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,19 +13,13 @@ import rest.markets.resources.ItinerantMarket;
 public class ItinerantMarketController {
 	
 	
+	
 	//crea un oggetto ItinerantMarket con le specifiche inserite nel Body
 	@PostMapping("/data")
 	public ResponseEntity<ItinerantMarket> filterItinerantMarket(@RequestBody ItinerantMarket itinerantMarket) {
 		
 		return new ResponseEntity(itinerantMarket, HttpStatus.OK);
-	}
 	
-	//tornai metadati della classe ItinerantMarket
-	@GetMapping("/metadata")
-	public ResponseEntity<Object> showMetadata(){
-	
-		return ResponseEntity(HttpStatus.OK);
-		
-	}
+		}
 		
 }
