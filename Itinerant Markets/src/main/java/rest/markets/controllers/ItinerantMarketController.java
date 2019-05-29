179b66960c04a3ae24703003e06701994c9afedd
ceciu <cecistecconi@gmail.com>
2019-05-30 00:56:21 +0200

    Adding a metadata controller

--- Itinerant Markets/src/main/java/rest/markets/controllers/ItinerantMarketController.java
+++ Itinerant Markets/src/main/java/rest/markets/controllers/ItinerantMarketController.java
@@ -10,10 +10,26 @@ import rest.markets.resources.ItinerantMarket;
 @RestController
 public class ItinerantMarketController {
 	
+	
+	//crea un oggetto ItinerantMarket con le specifiche inserite nel Body
 	@PostMapping("/data")
 	public ResponseEntity<ItinerantMarket> filterItinerantMarket(@RequestBody ItinerantMarket itinerantMarket) {
 		
 		return new ResponseEntity(itinerantMarket, HttpStatus.OK);
+	}
+	
+	//tornai metadati della classe ItinerantMarket
+	@GetMapping("/metadata")
+	public ResponseEntity<Object> showMetadata(){
+		@ResponseBody("comune : string\n"+
+					"provincia : string\n"+
+					"year : integer\n"+
+					"stats : ItinerantStats\n"+
+					"	ItinerantStats.total : integer\n"+
+					"	ItinerantStats.attivation : integer\n"+
+					"	ItinerantStats.cessation : integer\n");
+		return ResponseEntity(HttpStatus.OK);
+		
 	}
 		
 }
