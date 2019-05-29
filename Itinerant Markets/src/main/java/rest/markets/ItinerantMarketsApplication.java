30034eb4e268788175d5507e8927584d886e1f5b
GianMarcoZampa <48010448+GianMarcoZampa@users.noreply.github.com>
2019-05-29 09:20:32 +0200

    Project upload
    Uploading the created project from eclipse

---
+++ Itinerant Markets/src/main/java/rest/markets/ItinerantMarketsApplication.java
@@ -0,0 +1,13 @@
+package rest.markets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItinerantMarketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItinerantMarketsApplication.class, args);
+	}
+
+}
