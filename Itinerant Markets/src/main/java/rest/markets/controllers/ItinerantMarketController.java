e0b989cedff4bab34e960b671c65d3617a168f34
ceciu <cecistecconi@gmail.com>
2019-05-30 09:45:51 +0200

    Editing controller

--- Itinerant Markets/src/main/java/rest/markets/controllers/ItinerantMarketController.java
+++ Itinerant Markets/src/main/java/rest/markets/controllers/ItinerantMarketController.java
@@ -4,21 +4,29 @@ import java.util.Vector;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import rest.markets.resources.ItinerantMarket;
+import rest.markets.services.ItinerantMarketService;
 
 @RestController
 public class ItinerantMarketController {
 	
+	@Autowired
+	ItinerantMarketService itinerantMarketService;
 	
+	@GetMapping(path = "/data", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Vector<ItinerantMarket>> getAll(){
+		return new ResponseEntity<Vector<ItinerantMarket>>(itinerantMarketService.getAll(),HttpStatus.OK);
+	}
 	
 	//crea un oggetto ItinerantMarket con le specifiche inserite nel Body
-	@PostMapping("/data")
+	@PostMapping(path = "/data", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ItinerantMarket> filterItinerantMarket(@RequestBody ItinerantMarket itinerantMarket) {
 		
-		return new ResponseEntity(itinerantMarket, HttpStatus.OK);
+		return new ResponseEntity<ItinerantMarket>(itinerantMarket, HttpStatus.OK);
 	
 		}
 		
