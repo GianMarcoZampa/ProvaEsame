1ad811eed4626db544c5d42ad2048a6378468686
ceciu <cecistecconi@gmail.com>
2019-06-19 20:15:52 +0200

    Adding parts of java documentation

--- Itinerant Markets/src/main/java/rest/markets/controllers/ItinerantMarketController.java
+++ Itinerant Markets/src/main/java/rest/markets/controllers/ItinerantMarketController.java
@@ -18,13 +18,17 @@ import rest.markets.utils.statistics.FieldStatistic;
 import rest.markets.utils.filters.RequestConditionalFilter;
 import rest.markets.utils.filters.RequestLogicalFilter;
 
+/**The ItinerantMarketController is a class that launch the spring application using a rest controller and it creates all the requests
+ */
 @RestController
 public class ItinerantMarketController {
 	
 	@Autowired
 	ItinerantMarketService itinerantMarketService;
-	
-	// This method returns all the object in the service
+	/**This method returns all the object in the service
+	 * @return show all the data in the body response
+	 * @throws NullPointerException
+	 */
 	@GetMapping(path = "/data", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Vector<ItinerantMarket>> getAll() throws NullPointerException {
 		Vector<ItinerantMarket> itMarket= itinerantMarketService.getAll(); 
@@ -58,7 +62,10 @@ public class ItinerantMarketController {
 		return new ResponseEntity<Vector<ItinerantMarket>>(iMa, HttpStatus.OK);
 	}*/
 	
-	// This method returns a jsonSchema of the ItinerantMarket class
+	/** This method returns a jsonSchema of the ItinerantMarket class
+	 * @return metadata of the ItinerantMarketService class
+	 * @throws NullPointerException
+	 */
 	@GetMapping(path = "/metadata", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<JsonSchema> getMetadata() throws NullPointerException {
 		
@@ -67,8 +74,13 @@ public class ItinerantMarketController {
 		return new ResponseEntity<JsonSchema>(jSchema, HttpStatus.OK);
 	}
 	
-	// This method returns statistics of the requested fields (more than one argument must be
-	// separated with ',')
+
+	/**This method returns statistics using the interface itinerantMarketService
+	 * @see itinerantMarketService
+	 * @param field requested field(more than one argument must be separated with ',')
+	 * @return the statics for field 
+	 * @throws NullPointerException when field doesn't exist
+	 */
 	@GetMapping(path = "/stats", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Vector<FieldStatistic>> getStats(@RequestParam String field) 
 			throws NullPointerException {
@@ -77,8 +89,12 @@ public class ItinerantMarketController {
 		return new ResponseEntity<Vector<FieldStatistic>>(fStat, HttpStatus.OK);	
 	}
 
-	// This method requests a filter object for each field and it returns the complete list of
-	// itinerant markets with this filter
+	/**This method requests a filter object for each field and it returns the complete list of itinerant markets with this filter
+	 * 
+	 * @param filters 
+	 * @return
+	 * @throws ResourceNotFoundException
+	 */
 	@PostMapping(path = "/data/cfilter", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Vector<ItinerantMarket>> getConditionalFilter(@RequestBody Vector<RequestConditionalFilter> filters) 
 			throws ResourceNotFoundException {
