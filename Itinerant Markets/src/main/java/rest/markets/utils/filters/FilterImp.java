2317b885fb5a06886b08677c3ea92c0574bc5362
ceciu <cecistecconi@gmail.com>
2019-06-21 12:44:14 +0200

    Editing FilterImp javadoc

--- Itinerant Markets/src/main/java/rest/markets/utils/filters/FilterImp.java
+++ Itinerant Markets/src/main/java/rest/markets/utils/filters/FilterImp.java
@@ -16,7 +16,20 @@ import rest.markets.resources.ItinerantMarketStats;
 @Service
 public class FilterImp implements Filter {
 
-	// Greater than filter
+	/**
+	 * This method creates a vector of elements with the matches field are greater or equal 
+	 * than the input value "linf" (conditional filter)
+	 * @param linf is the lower limits of the value
+	 * @param equal is set as true if linf is included in the comparison,
+	 * false otherwise
+	 * @param data is the vector of elements you want to filter
+	 * @param field is the selected field for filtering
+	 * @return filteredMarket is a vector which contains all the elements in data with 
+	 * field element greater (or equal if equal is true) than linf
+	 * @throws IllegalArgumentException to indicate that a method has been passed an illegal 
+	 * or inappropriate argument.
+	 *@throws SecurityException when the user doesn't have the access to some file
+	 */
 	@Override
 	public Vector<ItinerantMarket> gt(int linf, boolean equal, Vector<ItinerantMarket> data, String field) {
 		
@@ -75,7 +88,21 @@ public class FilterImp implements Filter {
 		return filteredMarkets;
 	}
 
-	// Less than filter
+	/**
+	 * This method creates a vector of elements with the matches field are less or equal
+	 * than the input value "lsup" (conditional filter)
+	 * @param lsup is the upper limits of the value
+	 * @param equal is set as true if lsup is included in the comparison,
+	 * false otherwise
+	 * @param data is the vector of elements you want to filter
+	 * @param field is the selected field for filtering
+	 * @return filteredMarket is a vector which contains all the elements in data with 
+	 * field element greater (or equal if equal is true) than lsup
+	 * @throws IllegalArgumentException to indicate that a method has been passed an illegal 
+	 * or inappropriate argument.
+	 *@throws SecurityException when the user doesn't have the access to some file
+	 */
+	
 	@Override
 	public Vector<ItinerantMarket> lt(int lsup, boolean equal, Vector<ItinerantMarket> data, String field) {
 		
@@ -134,6 +161,22 @@ public class FilterImp implements Filter {
 		return filteredMarkets;
 	}
 
+	/**
+	 * This method creates a vector of elements with the matches field included in the parameter
+	 * linf and lsup (conditional filter)
+	 * @param linf is the lower limits of the value
+	 * @param lsup is the upper limits of the value
+	 * @param equal is set as true if lsup is included in the comparison,
+	 * false otherwise
+	 * @param data is the vector of elements you want to filter
+	 * @param field is the selected field for filtering
+	 * @return filteredMarket is a vector which contains all the elements in data with 
+	 * field element greater (or equal if equal is true) than lsup
+	 * @throws IllegalArgumentException to indicate that a method has been passed an illegal 
+	 * or inappropriate argument
+	 * @throws SecurityException when the user doesn't have the access to some file
+	 */
+	
 	@Override
 	public Vector<ItinerantMarket> bt(int linf, int lsup, boolean equal, Vector<ItinerantMarket> data, String field) {
 		Vector<ItinerantMarket> filteredMarkets = gt(linf, equal, data, field);
@@ -141,6 +184,16 @@ public class FilterImp implements Filter {
 		return filteredMarkets;
 	}
 
+	/**
+	 * This method creates a vector with all the elements in data that matches the ones in the requestedIM
+	 * @param requestedIM is the parameter of the filter
+	 * @param in if it's true then the elements that matches are added to the final vector, otrherwise 
+	 * the matched elements are deleted from the final vector
+	 * @param data vector that contains all the data to be filtered
+	 * @return filteredMarkets is a vector which contain only the data that matches the requestedIM if in 
+	 * is true, otherwise it contains only the data that doesn't match
+	 */
+	
 	@Override
 	public Vector<ItinerantMarket> in(ItinerantMarket requestedIM, boolean in, Vector<ItinerantMarket> data) {
 		Vector<ItinerantMarket> filteredMarkets = new Vector<ItinerantMarket>();
@@ -156,7 +209,13 @@ public class FilterImp implements Filter {
 
 	}
 
-	// This method returns the get method of the field and class requested if exists else it returns null
+	/**
+	 * Method that saves the requestedClass
+	 * @param requestedClass
+	 * @param field
+	 * @return requestedClass
+	 */
+	
 	public <T> Method getRequestedMethod(Class<T> requestedClass, String field) {
 		
 		String methodName = "get" + field.substring(0,1).toUpperCase() + field.substring(1);
